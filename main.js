706c979da358b513329a3cd07e1ed1cacb2b6971
scs0209 <110822847+scs0209@users.noreply.github.com>
2022-11-01 16:53:12 +0900

    JS: Home contact

--- main.js
+++ main.js
@@ -15,13 +15,21 @@ document.addEventListener('scroll', () => {
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-  console.log(event.target.dataset.link);
   const target = event.target;
   const link = target.dataset.link;
   if(link == null) {
     return;
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: "smooth"} );
+  scrollIntoView(link);
 })
+
+// Handle click on "contact me" button on home
+const contactBtn = document.querySelector('.home__contact');
+contactBtn.addEventListener('click', (event) => {
+  scrollIntoView('#contact');
+})
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: "smooth"});
+}
